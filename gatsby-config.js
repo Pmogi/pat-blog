fb99ccb73fbca9cd68a97f9d506840e777cae4e5
Pat Mogianesi <pmogianesi@outlook.com>
2022-08-03 18:39:51 -0400

    Disqus put in gatsby config twice by accident.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
-    "gatsby-plugin-disqus",
     {
       resolve: "gatsby-source-contentful",
       options: {
